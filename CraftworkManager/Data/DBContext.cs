f69e1b6b34954e12cc2b315e7886e3e3c18fed08
Guilherme077 <gui.pilz.2007@gmail.com>
2026-01-11 19:08:44 -0300

    Add EF Core and initial Products table migration

--- CraftworkManager/Data/DBContext.cs
+++ CraftworkManager/Data/DBContext.cs
@@ -1,6 +1,14 @@
-﻿namespace CraftworkManager.Data
+﻿using CraftworkManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CraftworkManager.Data
 {
-    public class DBContext
+    public class DBContext : DbContext
     {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
     }
 }
--- CraftworkManager/Program.cs
+++ CraftworkManager/Program.cs
@@ -1,15 +1,20 @@
+using CraftworkManager.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.  
 builder.Services.AddControllersWithViews();
+builder.Services.AddEntityFrameworkSqlServer().AddDbContext<DBContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.  
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.  
     app.UseHsts();
 }
 
@@ -21,7 +26,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+   name: "default",
+   pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
