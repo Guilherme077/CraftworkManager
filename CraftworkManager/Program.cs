a1271550baae648eeed743fafb51fda257af050c
Guilherme077 <gui.pilz.2007@gmail.com>
2026-01-21 20:20:46 -0300

    Add Dashboard controller and view

--- CraftworkManager/Program.cs
+++ CraftworkManager/Program.cs
@@ -7,17 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.  
 builder.Services.AddControllersWithViews();
-builder.Services.AddEntityFrameworkSqlServer().AddDbContext<DBContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
+builder.Services.AddRazorPages();
+//builder.Services.AddEntityFrameworkSqlServer().AddDbContext<DBContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
 
-builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<DBContext>();
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-{
-    options.Password.RequiredLength = 6;
-    options.Password.RequireNonAlphanumeric = true;
-}
-    ).AddEntityFrameworkStores<DBContext>().AddDefaultTokenProviders();
+//builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+//{
+//    options.Password.RequiredLength = 6;
+//    options.Password.RequireNonAlphanumeric = true;
+//}
+//    ).AddEntityFrameworkStores<DBContext>().AddDefaultTokenProviders();
 
 var app = builder.Build();
 
@@ -40,5 +41,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
    name: "default",
    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
